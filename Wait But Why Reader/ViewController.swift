11d5a2f7323bbb549ed6bb70440aea7f4f225955
Colin Warn <colinwarn@Colins-MacBook.local>
2017-06-09 21:56:05 -0700

    In App purchases added

--- Wait But Why Reader/ViewController.swift
+++ Wait But Why Reader/ViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 import AVFoundation
 import GoogleMobileAds
+import StoreKit
 
-class ViewController: UIViewController, UITextFieldDelegate, GADBannerViewDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, GADBannerViewDelegate, SKProductsRequestDelegate {
+    
+    
+    
 
     @IBOutlet weak var adView: UIView!
     @IBOutlet weak var removeAds: UIButton!
@@ -23,8 +27,25 @@ class ViewController: UIViewController, UITextFieldDelegate, GADBannerViewDelega
     
     @IBOutlet weak var bannerViewAd: GADBannerView!
     
+    var itemBeingPurchased = String()
+    
+    
+    
     @IBOutlet var siriButton: UIButton!
     var myURlString: String? = ""
+    
+    //In App purchase
+    let bonusRobotVoicesID = "bonusrobotvoices"
+    let removeAdsID = "removeadsiris"
+    
+    var productID = ""
+    var productsRequest = SKProductsRequest()
+    var iapProducts = [SKProduct]()
+    var bonusRobotVoicesKey = UserDefaults.standard.bool(forKey: "bonusrobotvoices")
+    var removeAdsKey = UserDefaults.standard.bool(forKey: "removeads")
+    
+    
+    //Voice
     
     var siriSound: AVAudioPlayer!
     
@@ -35,11 +56,22 @@ class ViewController: UIViewController, UITextFieldDelegate, GADBannerViewDelega
     
     
     var speechSynthesizer = AVSpeechSynthesizer()
+    var productIDs: Array<String> = ["bonusrobotvoices", "removeads"]
+    var productsArray: Array<SKProduct> = []
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        print("BONUS ROBOT KEY: \(bonusRobotVoicesKey)")
+        print("REMOVE ADS: \(removeAdsKey)")
+        
+        // Add/hide buttons here
+        
+        // Fetch IAP Products available
+        fetchAvailableProducts()
         
         
         let request = GADRequest()
@@ -71,6 +103,110 @@ class ViewController: UIViewController, UITextFieldDelegate, GADBannerViewDelega
         
     }
     
+    // MARK: - FETCH AVAILABLE IAP PRODUCTS
+    func fetchAvailableProducts()  {
+        
+        // Put here your IAP Products ID's
+        let productIdentifiers = NSSet(objects:
+            bonusRobotVoicesID, removeAdsID
+        )
+        
+        productsRequest = SKProductsRequest(productIdentifiers: productIdentifiers as! Set<String>)
+        productsRequest.delegate = self
+        productsRequest.start()
+    }
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        if (response.products.count > 0) {
+            iapProducts = response.products
+            
+            // 1st IAP Product (Consumable) ------------------------------------
+            let firstProduct = response.products[0] as SKProduct
+            
+            
+            
+            // 2nd IAP Product (Non-Consumable) ------------------------------
+            let secondProd = response.products[1] as SKProduct
+            
+            
+        }
+    }
+    
+    func canMakePurchases() -> Bool {  return SKPaymentQueue.canMakePayments()  }
+    
+    func purchaseMyProduct(product: SKProduct) {
+        if self.canMakePurchases() {
+            let payment = SKPayment(product: product)
+            SKPaymentQueue.default().add(self)
+            SKPaymentQueue.default().add(payment)
+            
+            print("PRODUCT TO PURCHASE: \(product.productIdentifier)")
+            productID = product.productIdentifier
+            
+            
+            // IAP Purchases dsabled on the Device
+        } else {
+            alertController(message: "You've disabled in app purchaes")
+        }
+    }
+    
+    func alertController(message: String) {
+        let alert = UIAlertController(title: "IAP Tutorial", message: message, preferredStyle: .actionSheet)
+        let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(ok)
+        alert.show(self, sender: nil)
+    }
+    
+    func paymentQueueRestoreCompletedTransactionsFinished(_ queue: SKPaymentQueue) {
+        // Set var of purchase to thing, then set key
+        if itemBeingPurchased == "ads" {
+            removeAdsKey = true
+            UserDefaults.standard.bool(forKey: "removeads")
+        } else {
+            bonusRobotVoicesKey = true
+            UserDefaults.standard.bool(forKey: "bonusrobotvoices")
+        }
+        
+        alertController(message: "You've successfully restored your purchase!")
+        
+    }
+    
+    
+    // MARK:- IAP PAYMENT QUEUE
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction:AnyObject in transactions {
+            if let trans = transaction as? SKPaymentTransaction {
+                switch trans.transactionState {
+                    
+                case .purchased:
+                    SKPaymentQueue.default().finishTransaction(transaction as! SKPaymentTransaction)
+                    
+                    // The Consumable product (10 coins) has been purchased -> gain 10 extra coins!
+                    if productID == bonusRobotVoicesID {
+                        
+                        // Do robot stuff
+                        
+                        
+                        
+                        // The Non-Consumable product (Premium) has been purchased!
+                    } else if productID == removeAdsID {
+                        
+                        //Remove adds stuff
+                    }
+                    
+                    break
+                    
+                case .failed:
+                    SKPaymentQueue.default().finishTransaction(transaction as! SKPaymentTransaction)
+                    break
+                case .restored:
+                    SKPaymentQueue.default().finishTransaction(transaction as! SKPaymentTransaction)
+                    break
+                    
+                default: break
+                }}}
+    }
+    
     @IBAction func modeChanged(_ sender: Any) {
         
         if botVoice.selectedSegmentIndex == 0 {
@@ -81,10 +217,14 @@ class ViewController: UIViewController, UITextFieldDelegate, GADBannerViewDelega
     }
     
     @IBAction func addVoices(_ sender: Any) {
+        itemBeingPurchased = "voices"
+        purchaseMyProduct(product: iapProducts[1])
     }
     
     
     @IBAction func removeAds(_ sender: Any) {
+        itemBeingPurchased = "ads"
+        purchaseMyProduct(product: iapProducts[0])
     }
     
     
@@ -118,6 +258,12 @@ class ViewController: UIViewController, UITextFieldDelegate, GADBannerViewDelega
         }
     }
     
+    @IBAction func restorePurchases(_ sender: Any) {
+        SKPaymentQueue.default().add(self)
+        SKPaymentQueue.default().restoreCompletedTransactions()
+
+        
+    }
 
     @IBAction func textEdited(_ sender: UITextField) {
         siriButton.alpha = 1
@@ -159,6 +305,20 @@ class ViewController: UIViewController, UITextFieldDelegate, GADBannerViewDelega
         
         
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
     
 
 
